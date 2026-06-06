f47ad8b8019d8f6d03ee730935bd83a0c93669c2
iagomuinos-code <iago.muinos.romero@gmail.com>
2026-06-06 15:59:00 +0100

    Add files via upload

---
+++ main.py
@@ -0,0 +1,84 @@
+import pygame
+pygame.init()
+
+
+width=900
+height=500
+screen = pygame.display.set_mode((width,height))
+
+bg_img = pygame.image.load("images\desertbgnew.jpg")
+# cowboyright_img = pygame.image.load("images\cowboyright.png")
+# cowboyleft_img
+
+#game variables
+plyersize = (40,60)
+playerspeed= 3
+maxbullet = 2
+bulletspeed = 8
+bulletsize= (10,4)
+hp = 100
+isrunning = True
+
+clock = pygame.time.Clock()
+fps = 60
+
+#slecting fonts
+hpfont = pygame.font.SysFont("helvetica",20)
+winnerfont = pygame.font.SysFont("timesnewroman",80)
+
+divider = pygame.Rect(width//2-5,0,10,height)
+
+
+#creating characters
+class Player(pygame.sprite.Sprite):
+    def __init__(self,x,y,image_path,controls,side):
+        #initialising the init function of parent class
+        super().__init__()
+        self.image = pygame.image.load(image_path)
+        self.image = pygame.transform.scale(self.image,(40,60))
+        #adding collider/hitbox around image
+        self.rect = self.image.get_rect(topleft=(x,y)) # topleft?
+        self.controls = controls
+        self.side = side
+        self.hp = hp
+        self.bullets = pygame.sprite.Group
+
+    def move(self,keys):
+        pass
+
+
+
+def draw():
+    screen.blit(bg_img,(0,0))
+    #display divide
+    pygame.draw.rect(screen,"black", divider)
+    pygame.display.update()
+
+
+
+def main():
+    isrunning = True
+    while isrunning: 
+        clock.tick(fps)
+        
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                isrunning = False
+                pygame.quit()
+                exit(0)
+
+        draw()
+
+main()
+
+        
+
+
+
+
+
+
+
+
+
+
